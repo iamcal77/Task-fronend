cd9bec04335343145278530e401d3c79f5b17d4f
Calvin Kipkirui <calvinkipkirui99@gmail.com>
2025-07-30 19:15:00 +0300

    deployed frontend

--- src/Forms/ProductForm.jsx
+++ src/Forms/ProductForm.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import {
   TextBox,
   NumberBox,
   SelectBox,
-  TextArea,
   Validator,
 
 } from 'devextreme-react';
